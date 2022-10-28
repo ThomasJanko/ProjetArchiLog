238752fa6f0d309a526c6328fd2a2a83eb564c20
thomas78125 <thomas78125@gmail.com>
2022-10-28 11:15:17 +0200

    Product Model + Controller + migration

--- ProjetArchiLog/Controllers/ProductsController.cs
+++ ProjetArchiLog/Controllers/ProductsController.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using LibraryArchiLog.Controllers;
+using ProjetArchiLog.data;
+using ProjetArchiLog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace ProjetArchiLog.Controllers
 {
-    public class ProductsController
+    [Route("/api/[controller]")]
+
+    public class ProductsController: BaseController<ArchiLogDbContext, Product>
     {
-        public ProductsController()
+        public ProductsController(ArchiLogDbContext context) : base(context)
         {
-            //TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTtt
-            
+
         }
     }
 }
