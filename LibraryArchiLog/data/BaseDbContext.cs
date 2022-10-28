e1f502068135d5e0516eb500eabf3d0b6cb8f2a3
thomas78125 <thomas78125@gmail.com>
2022-10-28 09:24:07 +0200

    Library Base

--- LibraryArchiLog/data/BaseDbContext.cs
+++ LibraryArchiLog/data/BaseDbContext.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using ArchiLibrary.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+using EntityState = Microsoft.EntityFrameworkCore.EntityState;
 
 namespace LibraryArchiLog.data
 {
-    internal class BaseDbContext
+    public class BaseDbContext : DbContext
     {
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellation = default)
+        {
+            ChangeCreatedState();
+            ChangeDeletedState();
+            return base.SaveChangesAsync(cancellation);
+        }
+
+        private void ChangeCreatedState()
+        {
+            var createdEntities = ChangeTracker.Entries().Where(x => x.State == EntityState.Added);
+            foreach (var item in createdEntities)
+            {
+                if (item.Entity is BaseModel model)
+                {
+                    model.Active = true;
+                    model.CreatedAt = DateTime.Now;
+                    model.DeletedAt = null;
+                }
+            }
+        }
+
+        private void ChangeDeletedState()
+        {
+            var deletedEntries = ChangeTracker.Entries().Where(x => x.State == EntityState.Deleted);
+            foreach (var item in deletedEntries)
+            {
+                if (item.Entity is BaseModel model)
+                {
+                    model.Active = false;
+                    model.DeletedAt = DateTime.Now;
+                    item.State = EntityState.Modified;
+                }
+            }
+        
     }
+}
 }
