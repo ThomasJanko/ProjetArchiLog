772bd554d0db11e8b47418fcbc7e023218ab09b1
thomas78125 <thomas78125@gmail.com>
2022-10-28 09:48:45 +0200

    library Basee

---
+++ ProjetArchiLog/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LibraryArchiLog.Models;
+
+
+namespace ProjetArchiLog.Models
+{
+    public class Brand : BaseModel
+    {
+        //[Key]
+        //public int ID { get; set; }
+        //public DateTime CreatedAt { get; set; }
+        [StringLength(50)]
+        [Required()]
+        public string? Name { get; set; }
+
+        //[Column("Name=nameColumn")]
+        public string? Slogan { get; set; }
+
+        //public List<Car>? Cars { get; set; }
+        //public IEnumerable<Car> Cars { get; set; } boucle infini pour l'Api  
+
+    }
+}
