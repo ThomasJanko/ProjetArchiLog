c56a509d22a98a1f517331734fe36098b8762f0b
Djibril-6et <djibril.6et@gmail.com>
2022-11-03 12:35:59 +0100

    init sortby feature

--- LibraryArchiLog/Extensions/QueryExtension.cs
+++ LibraryArchiLog/Extensions/QueryExtension.cs
@@ -11,7 +11,7 @@ namespace LibraryArchiLog.Extensions
 {
     public static class QueryExtension
     {
-        public static IOrderedQueryable<TModel> Sort<TModel>(this IQueryable<TModel> query, Params p)
+        public static IOrderedQueryable<TModel> Sort<TModel>(this IQueryable<TModel> query, SortParams p)
         {
             if (!string.IsNullOrWhiteSpace(p.Asc))
             {
@@ -28,9 +28,27 @@ namespace LibraryArchiLog.Extensions
                 return query.OrderBy(lambda);
                 //return query.OrderBy(x => x.Name);
             }
-            else
-                return (IOrderedQueryable<TModel>)query;
 
+            else if (!string.IsNullOrWhiteSpace(p.Desc))
+            {
+                string champ = p.Desc;
+
+                //créer lambda
+                var parameter = Expression.Parameter(typeof(TModel), "x");
+                var property = Expression.Property(parameter, champ/*"Name"*/);
+
+                var o = Expression.Convert(property, typeof(object));
+                var lambda = Expression.Lambda<Func<TModel, object>>(o, parameter);
+
+                //utilisation lambda
+                return query.OrderByDescending(lambda);
+                //return query.OrderBy(x => x.Name);
+            }
+
+            else
+            {
+                return (IOrderedQueryable<TModel>)query;
+            }
         }
 
         //public static IOrderedQueryable<TModel> Filter<TModel>(this IQueryable<TModel> query, Params p )
---
+++ LibraryArchiLog/Models/SortParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryArchiLog.Models
+{
+    public class SortParams
+    {
+        public string? Asc { get; set; }
+        public string? Desc { get; set; }
+    }
+}
+
